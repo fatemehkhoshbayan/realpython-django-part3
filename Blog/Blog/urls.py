cd4a807068494d4233ddb3aa8c8316329c275aa1
Fatemeh <f.khoshbayan@gmail.com>
2022-02-08 18:51:31 +0300

    Update core application

--- Blog/Blog/urls.py
+++ Blog/Blog/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from core import views as core_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', core_views.listing, name='listing'),
+    path('view_blog/<int:blog_id>', core_views.view_blog, name='view_blog'),
+    path('see_request/', core_views.see_request),
+    path('user_info/', core_views.user_info),
 ]
