5695284df09473868d6c90e861ab539c0bc73ddc
Fatemeh <f.khoshbayan@gmail.com>
2022-02-21 17:54:05 +0300

    Dockerize the project

--- Blog/Blog/settings.py
+++ Blog/Blog/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'Blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'admin',
+        'PASSWORD': '1234',
+        'HOST': '0.0.0.0',
+        'PORT': '5432'
     }
 }
 
